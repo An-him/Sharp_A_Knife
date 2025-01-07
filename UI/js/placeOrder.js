7d2313d191dd0d6de89ae1c9d465d0f77354f691
bstwagir@mtu.edu <bstwagir@mtu.edu>
2025-01-07 16:03:54 +0200

    feat: add dashboard page

--- UI/js/placeOrder.js
+++ UI/js/placeOrder.js
@@ -9,4 +9,34 @@ function updateKnifeCount(value) {
     console.log(numberKnives);
     localStorage.setItem('numberKnives', JSON.stringify(numberKnives));
     location.href="pages/payment.html"
-  }  
+  }
+  
+  $(document).ready(function() {
+    $('#contact-form').on('submit', function(event) {
+      event.preventDefault();
+
+      var formData = {
+          name: $('#name').val(),
+          email: $('#email').val(),
+          message: $('#message').val(),
+      };
+      $('#contact-btn').text("Sending...");      
+
+      $.ajax({
+          type: 'POST',
+          url:  'https://team-furebo-e-commerce-bn.onrender.com/api/login',
+          data: JSON.stringify(formData),
+          contentType: 'application/json',
+          success: function(response) {
+              alert('Payment successful!');
+              $('#contact-btn').text("Sent!");
+              $('#contact-btn').css("color","green");
+          },
+          error: function(error) {
+              alert('An error occurred during checkout. Please try again.');
+              $('#contact-btn').text("Error");
+              $('#contact-btn').css("color","yellow");             
+          }
+        });
+      });
+    });
