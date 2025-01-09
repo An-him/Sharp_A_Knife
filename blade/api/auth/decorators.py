b2ea0c88bc65f6238653a38c35e564f1bc6f8401
Yahweh's Delight <intricatesyllable@gmail.com>
2025-01-09 15:18:04 +0300

    Update .gitignore to exclude certain files

--- blade/api/auth/decorators.py
+++
@@ -1,8 +0,0 @@
-from functools import wraps
-from flask_jwt_extended import get_current_user
-
-def auth_role():
-    def wrapper(fn):
-        @wraps(fn)
-        def decorato(*args, **kwargs):
-            current_user = get_current_user()
--- blade/api/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-# creates an instance of the SQLAlchemy class
-db=SQLAlchemy()
-
-"""
-"""
