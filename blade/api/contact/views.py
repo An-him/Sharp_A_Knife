db457121b3c25406ab8688e901f183ee0056fae3
Yahweh's Delight <intricatesyllable@gmail.com>
2025-01-09 14:16:19 +0300

    Namespace related to contact info

---
+++ blade/api/contact/views.py
@@ -0,0 +1,31 @@
+from flask_restx import Namespace, fields, Resource
+from http import HTTPStatus
+from ..models.contactForm import ContactForm
+
+contact_namespace=Namespace("contact", description="Namespace for Contact Form")
+
+contactForm_model=contact_namespace.model("Contact",{
+    "fullname":fields.String(required=True, description="Full Name"),
+    "email":fields.String(required=True, description="Email Address"),
+    "message":fields.String(required=True, description="Message")
+})
+
+@contact_namespace.route("/contactform")
+class ContactResource(Resource):
+
+
+    @contact_namespace.expect(contactForm_model)
+    @contact_namespace.marshal_with(contactForm_model)
+    def post(self):
+        data=contact_namespace.payload
+        new_contact=ContactForm(
+            fullname=data['fullname'],
+            email=data['email'],
+            message=data['message']
+        )
+        new_contact.save()
+        return new_contact, HTTPStatus.CREATED
+    
+    @contact_namespace.marshal_with(contactForm_model)
+    def get(self):
+        return ContactForm.query.all(), HTTPStatus.OK
