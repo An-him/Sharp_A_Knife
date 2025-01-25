7c5f0c346f3535514c98801053d870e5d8f5bfa9
Am-Him <121066692+am-muhwezi@users.noreply.github.com>
2025-01-26 01:47:20 +0300

    Update __init__.py
    removed all routes pointing to the frontend

--- blade/api/__init__.py
+++ blade/api/__init__.py
@@ -21,18 +21,9 @@ from werkzeug.exceptions import NotFound, MethodNotAllowed
 
 def create_app(config=config_dict['dev']):
 
-    blade_dir=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    project_root=os.path.dirname(blade_dir)
-
-
-    static_folder = os.path.join(project_root, 'UI')
 
     
-    app=Flask(__name__, static_folder=static_folder, static_url_path='')
-    print(f"Checking index.html at: {os.path.join(app.static_folder, 'index.html')}")
-    print(f"App static folder: {app.static_folder}")
-    print(f"App static url path: {app.static_url_path}")
-
+    app=Flask(__name__)
 
     
     CORS(app)
@@ -59,7 +50,6 @@ def create_app(config=config_dict['dev']):
             description='REST API for Kitchen Blade Services',
             authorizations='authorizations',
             security='Bearer Auth',
-            doc='/docs',
             version='1.0',
             )
 
@@ -77,9 +67,6 @@ def create_app(config=config_dict['dev']):
     def method_not_allowed(error):
         return {'error':'Method not allowed'},405
 
-    @app.route('/test')
-    def test():
-        return {'message': 'Test route working'}, 200
 
     api.add_namespace(order_namespace, path='/orders')
     api.add_namespace(auth_namespace, path='/auth')
@@ -87,14 +74,6 @@ def create_app(config=config_dict['dev']):
     api.add_namespace(users_namespace, path='/users')
 
 
-
-    @app.route("/")
-    def index():
-        try:
-            return app.send_from_directory(app.static_folder, 'index.html')
-        except Exception as e:
-            print(f"Error serving index.html: {str(e)}")
-            return {"error": str(e)}, 500
 
 
     @app.shell_context_processor
